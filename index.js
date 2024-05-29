3aa38d52afcb66f79ff0dd4c55faec511801d04b
Archeb <archebasic@hotmail.com>
2024-05-30 00:19:27 +0800

    specify cipher suite

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { v4: uuidv4 } = require("uuid");
 const { ProxyAgent } = require("proxy-agent");
 const agent = new ProxyAgent();
 const app = express();
+const https = require("https");
 const axios = require("axios");
 const port = process.env.PORT || 8080;
 const validApiKey = process.env.PASSWORD;
@@ -81,6 +82,9 @@ app.post("/v1/messages", apiKeyAuth, (req, res) => {
 				headers: {
 					"User-Agent": session.user_agent,
 					"Cookie": session.cookie,
+					httpsAgent: new https.Agent({
+						ciphers: "TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA:AES256-SHA",
+					}),
 				},
 				httpsAgent: agent,
 			});
